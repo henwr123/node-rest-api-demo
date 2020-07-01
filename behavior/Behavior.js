ae12444946b43326d41e4560fd8393fc9501edb0
William Hendrick <william.hendrick@gmail.com>
2020-07-01 18:33:57 -0500

    Better validations for function using Object ref

--- behavior/Behavior.js
+++ behavior/Behavior.js
@@ -5,33 +5,40 @@ const BehaviorScheme = new mongoose.Schema({
 
     name: { 
         type: String, 
-        required: true 
+        trim: true,
+        required: true
     },
 
     description: String,
 
+    // function_id: {
+    //     type: String,
+    //     required: true,
+    //     validate: {
+    //         validator: function (value) {
+
+    //             return new Promise(function (resolve, reject) {
+
+    //                 // Validate the function exists
+    //                 Function.findById(value, function (err, func) {
+    //                     if (err) {
+    //                         resolve(false);
+    //                     }
+    //                     else {
+    //                         resolve(true);
+    //                     }
+    //                 });
+
+    //             });
+    //         }
+    //     }
+    // },
+
     function_id: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (value) {
-
-                return new Promise(function (resolve, reject) {
-
-                    // Validate the function exists
-                    Function.findById(value, function (err, func) {
-                        if (err) {
-                            resolve(false);
-                        }
-                        else {
-                            resolve(true);
-                        }
-                    });
-
-                });
-            }
-        }
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Function'
     }
+
 });
 
 mongoose.model('Behavior', BehaviorScheme);
